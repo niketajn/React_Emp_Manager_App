c3c2e74d4cf47afcf3ec9d4c942f25518334acf7
niketajain <niketa.jain@metamug.com>
2022-09-30 19:08:37 +0530

    used useForm hook for validation

--- src/Components/AddEmployee.js
+++ src/Components/AddEmployee.js
@@ -1,74 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React  from 'react';
 import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 const AddEmployee = (props) =>{
-    const [empInfo,setEmpInfo] = useState({
-        name:'',
-        email:''
-    });
 
-    const [errorMessage1,setErrorMessage] = useState({
-        nameInputError:'',
-        emailInputError:''
-    });
+    const {register, handleSubmit, formState:{errors}, trigger } = useForm();
 
     const navigate = useNavigate();
-
-    const add = (e) => {
-        e.preventDefault();
-        if(validate()){
-            navigate("/list");
-        props.addEmployeeHandler(empInfo);
-        setEmpInfo({name:'',email:''});
-        };
+    
+    const onSubmit = (data) => {
+        props.addEmployeeHandler(data);
+        navigate("/list");
     }
-
-    const validate=() => {
-        let nameError = "";
-        let emailError = "";
-        let nameRegex = new RegExp("^[a-zA-Z \s]+$")
-        
-        if (empInfo.name==="" || nameRegex.test(empInfo.name)===false ) {
-            nameError = "Please enter a valid name";
-          }
-
-        const emailRegex = /\S+@\S+\.\S+/;
-        if (empInfo.email === "" || emailRegex.test(empInfo.email) === false) {
-            emailError = "Please enter a valid email";
-          }
-
-          if(nameError || emailError){
-              setErrorMessage(
-                {...errorMessage1,
-                    nameInputError:nameError,
-                    emailInputError:emailError
-                }
-            );
-            return false;
-          }
-          return true;
-    };
-
 
     return(
         <div className="ui main">
                 <h2 style={{marginTop:"2em"}}>Add Employee Details</h2>
-                <form className="ui form" onSubmit={add}>
+                <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
                     <div className="field">
                         <label>Name</label>
                         <input type="text" name="name" placeholder="name" 
-                        onChange={e=>setEmpInfo({...empInfo,name:e.target.value})} required
+                        {...register('name', 
+                        { 
+                            required: "Username is required",
+                            minLength: {
+                                value: 10,
+                                message: "Minimum Required length is 10",
+                            },
+                            maxLength:{
+                                value:20,
+                                message: "Maximum required length is 20"
+                            }
+                        }
+                        )}
+                        onKeyUp={() => {
+                            trigger("name");
+                          }}
                         />
-                        <p style={{color:"red"}}>{errorMessage1.nameInputError}</p>
+                        
                     </div>
+                    <p className="errorMsg">{errors.name?.message}</p>
                     
                     <div className="field">
                         <label>Email</label>
                         <input type="text" name="email" placeholder="Email" 
-                        onChange={e=>setEmpInfo({...empInfo,email:e.target.value})} required
+                        {...register('email', 
+                            { required: "Email is required",
+                              pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Please enter valid email."
+                            }
+                            }
+                            )}
+                        onKeyUp={() => {
+                            trigger("email");
+                          }}
                         />
-                        <p style={{color:"red"}}>{errorMessage1.emailInputError}</p>
                     </div>
+                    <p className="errorMsg">{errors.email?.message}</p>
                     <button className="ui button blue">Add</button>
                 </form>
             </div>
